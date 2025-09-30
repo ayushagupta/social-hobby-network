83ca0d884be9a5c6bfc35178eda7782bbac0d355
Ayush Gupta <mumbai.ayushgupta@gmail.com>
2025-09-30 15:32:53 -0400

    setup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,48 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProfilePage from "./pages/ProfiePage";
+import GroupsPage from "./pages/GroupsPage";
+import GroupDetailPage from "./pages/GroupDetailPage";
+import { useSelector } from "react-redux";
+
+function PrivateRoute({ children }) {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  return isLoggedIn ? children : <Navigate to="/login" />;
+}
 
 function App() {
-
-  return <p>Hello World</p>;
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route
+        path="/profile"
+        element={
+          <PrivateRoute>
+            <ProfilePage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/groups"
+        element={
+          <PrivateRoute>
+            <GroupsPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/groups/:id"
+        element={
+          <PrivateRoute>
+            <GroupDetailPage />
+          </PrivateRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
+  );
 }
 
 export default App;
